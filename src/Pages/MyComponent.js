ff1e4093e7d5698bbde448a8bb4e419cbc74803b
ARSHIOSDK <arshiosdk@gmail.com>
2024-03-21 11:59:04 +0100

    Crated First component with css and implimented it

---
+++ src/Pages/MyComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './MyComponent.css'; 
+
+const MyComponent = () => {
+  return (
+    <div className="container">
+      <h1>Hello, React!</h1>
+      <p>This is my first component.</p>
+    </div>
+  );
+}
+
+export default MyComponent;
